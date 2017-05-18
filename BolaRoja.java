49b000e3ef05d9cf7a99a3a13d5cd81b00274fac
Valeriesantos <valnika96@gmail.com>
2017-05-18 12:59:15 +0200

    Bola se mueve hacia derecha

--- BolaRoja.java
+++ BolaRoja.java
@@ -4,9 +4,17 @@ import javafx.scene.Scene;
 import javafx.stage.Stage; 
 import javafx.scene.shape.Circle; 
 import javafx.scene.paint.Color; 
+import javafx.animation.Timeline;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import java.lang.Object;
+import javafx.util.Duration;
+import javafx.scene.transform.Translate;
+import javafx.animation.AnimationTimer;
 
 public class BolaRoja extends Application { 
-
     public static void main(String args[]) { 
         launch(args) ; 
     } 
@@ -15,28 +23,35 @@ public class BolaRoja extends Application {
     public void start(Stage stage) { 
 
         Circle circle = new Circle() ; 
+        final Timeline timeline = new Timeline();
+        timeline.setCycleCount(Timeline.INDEFINITE);
 
-
-      
         circle.setRadius(50.0f) ; 
         circle.setCenterX(250.0f);
         circle.setCenterY(250.0f);
 
+        final KeyFrame kreyframe = new KeyFrame(Duration.millis(20), 
+                new EventHandler<ActionEvent>() {
+                    @Override public void handle(ActionEvent t) {
+                        circle.setCenterX(circle.getTranslateX() + circle.getCenterX());
+                        circle.setCenterY(circle.getTranslateY() + circle.getCenterY());
+                        circle.setTranslateX(5);
+                        
 
+                    }
+                });
+
+        timeline.getKeyFrames().add(kreyframe);
         Group root = new Group(circle) ; 
-
 
         Scene scene = new Scene(root, 500, 500) ;  
 
         stage.setTitle("Circle") ; 
-        circle.setFill(Color.RED) ;  
+        circle.setFill(Color.RED) ; 
 
-
-
-        stage.setScene(scene) ; 
-
-
+        stage.setScene(scene); 
         stage.show();
+        timeline.play();
     }
 } 
 
