747d24035ba17f341d9773d4e73385d8233c05a4
Valeriesantos <valnika96@gmail.com>
2017-05-25 13:04:34 +0200

    Game over

--- BolaRoja.java
+++ BolaRoja.java
@@ -19,11 +19,16 @@ import java.util.Random;
 import javafx.scene.shape.Rectangle; 
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.KeyCode;
-
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;;
+import javafx.scene.control.Label;
 public class BolaRoja extends Application { 
-    private int velocidadX = 1;
-    private int velocidadY = 1;
-    private int velocidadxRectangle = 2;
+    private int velocidadX = 2;
+    private int velocidadY = 2;
+    private int velocidadxRectangle = 5;
+    private static int RADIO = 20;
+     private static int DIAMETRO = 40;
+    private   Timeline keyframe;
 
     public static void main(String args[]) { 
         launch(args) ; 
@@ -33,56 +38,87 @@ public class BolaRoja extends Application {
     public void start(Stage stage) { 
 
         Circle circle = new Circle() ;
-        Rectangle rectangle = new Rectangle(150,450,100,20) ;
+        Rectangle rectangle = new Rectangle(150,450,100,15) ;
         rectangle.setFill(Color.BLUE) ; 
+        rectangle.setArcWidth(10);
+        rectangle.setArcHeight(10);
 
-        Group root = new Group(circle , rectangle) ;
+        Image image = new Image("game-over.png");
+        ImageView iv = new ImageView();
+
+        Group root = new Group(circle , rectangle ,iv ) ;
         Scene scene = new Scene(root, 500, 500) ;  
         Random randomNumbers = new Random();
 
-        circle.setRadius(20.0f) ; 
-        circle.setCenterX(20+randomNumbers.nextInt(350)) ;
-        circle.setCenterY(20+randomNumbers.nextInt(350));
+        circle.setRadius(RADIO) ; 
+        circle.setCenterX(RADIO +randomNumbers.nextInt(350)) ;
+        circle.setCenterY(RADIO +randomNumbers.nextInt(350));
 
-        Timeline keyframe = new Timeline(new KeyFrame(Duration.millis(10), 
-                    new EventHandler<ActionEvent>() {
-                        @Override public void handle(ActionEvent t) {
+        keyframe = new Timeline(new KeyFrame(Duration.millis(10), 
+                new EventHandler<ActionEvent>() {
+                    @Override public void handle(ActionEvent t) {
 
-                            double xMin = circle.getBoundsInParent().getMinX();
-                            double yMin = circle.getBoundsInParent().getMinY();
-                            double xMax = circle.getBoundsInParent().getMaxX();
-                            double yMax = circle.getBoundsInParent().getMaxY();
+                        double xMin = circle.getBoundsInParent().getMinX();
+                        double yMin = circle.getBoundsInParent().getMinY();
+                        double xMax = circle.getBoundsInParent().getMaxX();
+                        double yMax = circle.getBoundsInParent().getMaxY();
+                        rectangle.setTranslateX(rectangle.getTranslateX() +velocidadxRectangle);
 
-                            if (xMin < 0 || xMax > scene.getWidth()) {
-                                velocidadX = velocidadX * -1;
-                            }
-                            if (yMin < 0) {
-                                velocidadY = velocidadY * -1;
-                            }
-                            
-                            if(circle.getBoundsInParent().intersects(rectangle.getBoundsInParent())){
-                                velocidadY = velocidadY * -1;
-                            }
-
-                            circle.setTranslateX(circle.getTranslateX() + velocidadX);
-                            circle.setTranslateY(circle.getTranslateY() + velocidadY);
-                            rectangle.setTranslateX(rectangle.getTranslateX() +velocidadxRectangle);
+                        if (xMin < 0 || xMax > scene.getWidth()) {
+                            velocidadX = velocidadX * -1;
                         }
-                    }));
+
+                        if (yMin < 0) {
+                            velocidadY = velocidadY * -1;
+                        }
+
+                        if(circle.getBoundsInParent().intersects(rectangle.getBoundsInParent())){
+                            velocidadY = velocidadY * -1;
+                        }
+
+                        if( yMax > scene.getWidth() + DIAMETRO){
+                            iv.setX(200);
+                            iv.setY(200);
+                            iv.setFitHeight(100);
+                            iv.setFitWidth(100);
+                            iv.setImage(image);  
+                            keyframe.stop();
+
+                        }
+                        double xMinR = rectangle.getBoundsInParent().getMinX();
+                        double xMaxR = rectangle.getBoundsInParent().getMaxX();
+
+                        if(xMinR <= 0 || xMaxR  >= scene.getWidth()){
+                            velocidadxRectangle = 0;                                
+
+                        }
+                        circle.setTranslateX(circle.getTranslateX() + velocidadX);
+                        circle.setTranslateY(circle.getTranslateY() + velocidadY);
+                    }
+                }));
 
         keyframe.setCycleCount(Timeline.INDEFINITE);
         stage.setTitle("Circle") ; 
         circle.setFill(Color.RED) ; 
 
         scene.setOnKeyPressed(event -> {             
-                if(event.getCode() == KeyCode.RIGHT ){
-                    velocidadxRectangle =  1;
+                if(event.getCode() == KeyCode.RIGHT )
+                {
+
+                    if(rectangle.getBoundsInParent().getMaxX()  <500  ){
+                        velocidadxRectangle = 3;
+                    }
+
                 }
 
-                else if(event.getCode() == KeyCode.LEFT){
-                    velocidadxRectangle =  -1; //Cambiamos direccion del rectangulo
-                }
+                else if(event.getCode() == KeyCode.LEFT)
+                {
 
+                    if(rectangle.getBoundsInParent().getMinX()  > 0){
+                        velocidadxRectangle = -3;
+                    }
+
+                }
                 rectangle.setTranslateX(rectangle.getTranslateX() + velocidadxRectangle);
                 event.consume();
             });
